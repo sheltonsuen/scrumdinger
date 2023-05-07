f2500a2a7e6ffeddffbe72f558f97e54358c45c7
sheltonsuen <sheltonsuen@gmail.com>
2023-05-07 22:49:50 +0800

    rename

--- scrumdinger/MeetingView.swift
+++ scrumdinger/MeetingView.swift
@@ -19,7 +19,7 @@ struct MeetingView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
         MeetingView()
     }
