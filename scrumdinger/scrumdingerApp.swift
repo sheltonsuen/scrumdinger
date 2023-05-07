37b1d9cd7e3e0f7923cdc584566546f67e0526dd
sheltonsuen <sheltonsuen@gmail.com>
2023-05-07 22:26:31 +0800

    Initial Commit

---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Shelton Suen on 2023/5/7.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
